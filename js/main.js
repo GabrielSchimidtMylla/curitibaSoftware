621992e3a631831338c10403891336caa3ed067a
Robson Moura <robson@socarrao.com.br>
2016-10-21 16:11:03 -0200

    adicionando no efeito canvas -Gabriel-

--- js/main.js
+++ js/main.js
@@ -1,65 +1,143 @@
 new WOW().init();
-$(function () {
-  var cores = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'];
-  var minSpeed = .02;
-  var maxSpeed = .03;
-  var varSpeed = .01;
-  var counter = 0;
-  var counter2 = 2;
+(function () {
+    //Constantes
+    var canvas;
+    var ctx;
+    var dx = 1;
+    var dy = 1;
+    //Variaveis
+    var WIDTH = 500;
+    var HEIGHT = 150;
+    var cores = ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'];
+    var balls = new Array();
+    var quantidadeBolinhas = 10;
+    var tamanhoMinimo = 3;
+    var tamanhoMaximo = 6;
+    var velocidade = 50;
 
-  function startBounce(element) {
-    var container = element.parent();
-    var width = container.innerWidth() - element.outerWidth();
-    var height = container.innerHeight() - element.outerHeight();
-    var vertSpeed = ((Math.random() * (maxSpeed - minSpeed)) + minSpeed);
-    var horzSpeed = ((Math.random() * (maxSpeed - minSpeed)) + minSpeed);
-    bounce(element, vertSpeed, height, 'top');
-    bounce(element, horzSpeed, width, 'left');
-  }
+    function tamanhoAleatorioBolinha() {
+        return Math.floor(Math.random() * (tamanhoMaximo - tamanhoMinimo + 1)) + tamanhoMinimo;
+    }
 
-  function bounce(element, speed, max, dir) {
-    speed += ((Math.random() * varSpeed) - (varSpeed / 2));
-    speed = speed < minSpeed ? minSpeed : (speed > maxSpeed ? maxSpeed : speed)
-    var time = max / speed;
-    var position = element.position();
-    if (position[dir] < 10) {
-      target = max;
+    function posicaoInicialAleatoriaX() {
+        return Math.floor(Math.random() * (WIDTH - 0 + 1));
     }
-    else {
-      target = 0;
-    }
-    var style = {
-      queue: false
-    };
-    style[dir] = target;
-    element.animate(style, {
-      duration: time
-      , queue: false
-      , easing: "linear"
-      , complete: function () {
-        counter2 = counter2 + 1;
-        if (counter2 <= 50000) bounce(element, time, max, dir);
-      }
-    });
-  }
 
-  function criarBolinha() {
-    counter = counter + 1;
-    var left = ($("body").innerWidth() * Math.random()) - 50;
-    var top = ($("body").innerHeight() * Math.random()) - 50;
-    var body = $("body");
-    var ball = document.createElement("span");
-    ball.style.backgroundColor = cores[(Math.random() * cores.length) | 0];
-    ball.className = "circle animated fadeIn";
-    ball.style.left = left + 'px';
-    ball.style.top = top + 'px';
-    body.append(ball);
-    startBounce($(ball));
-    if (counter <= 10) {
-      setTimeout(function () {
-        criarBolinha();
-      }, 20);
+    function posicaoInicialAleatoriaY() {
+        return Math.floor(Math.random() * (HEIGHT - 0 + 1));
     }
-  }
-  criarBolinha();
-});
+
+    function calculaRota(posicaoX, posicaoY, controleX, controleY) {
+        var restaX = (WIDTH - 10) - posicaoX;
+        var restaY = (HEIGHT - 10) - posicaoY;
+
+        if (restaX > 0 && controleX)
+            controleX = true;
+        else
+            controleX = false;
+
+        if (controleX)
+            posicaoX += dx;
+        else {
+            posicaoX -= dx;
+            if (posicaoX == 0)
+                controleX = true;
+        }
+
+        if (restaY > 0 && controleY)
+            controleY = true;
+        else
+            controleY = false;
+
+        if (controleY)
+            posicaoY += dy;
+        else {
+            posicaoY -= dy;
+            if (posicaoY == 0)
+                controleY = true;
+        }
+
+        return {
+            x: posicaoX,
+            y: posicaoY,
+            alternateX: controleX,
+            alternateY: controleY
+        }
+    }
+
+    function Desenhar() {
+
+        for (var i = 0; i < quantidadeBolinhas; i++) {
+
+            //Busca o elemento.
+            var ball = balls.slice(i)[0];
+
+            //Calcula a tracado.
+            var rota = calculaRota(ball.x, ball.y, ball.alternateX, ball.alternateY);
+
+            //Atualiza os valores do elemento.
+            ball.x = rota.x;
+            ball.y = rota.y;
+            ball.alternateX = rota.alternateX;
+            ball.alternateY = rota.alternateY;
+
+            //Realiza o desenho.
+            ctx.beginPath();
+            ctx.fillStyle = ball.color;
+            ctx.arc(rota.x, rota.y, ball.tamanho, 0, Math.PI * 2, true);
+            ctx.fill();
+        }
+    }
+
+    function LimparTela() {
+        ctx.fillStyle = "white";
+        ctx.strokeStyle = "white";
+        ctx.beginPath();
+        ctx.rect(0, 0, WIDTH, HEIGHT);
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+    }
+
+    function criarBolinhas() {
+
+        for (var i = 0; i < quantidadeBolinhas; i++) {
+            var a = posicaoInicialAleatoriaX();
+            var b = posicaoInicialAleatoriaY();
+            var c = ((Math.floor(Math.random()) % 2) == 0);
+            var d = cores[(Math.random() * cores.length) | 0];
+            var e = tamanhoAleatorioBolinha();
+
+            balls.push({
+                x: a,
+                y: b,
+                alternateX: c,
+                alternateY: c,
+                color: d,
+                tamanho: e
+            });
+        }
+    }
+
+    function Iniciar() {
+
+        var body = document.getElementsByTagName("body")[0];
+        WIDTH = body.clientWidth - 10;
+        HEIGHT = body.clientHeight - 10;
+
+        canvas = document.getElementById("canvas");
+        canvas.width = body.clientWidth;
+        canvas.height = body.clientHeight;
+        ctx = canvas.getContext("2d");
+        criarBolinhas();
+        return setInterval(Atualizar, velocidade);
+    }
+
+    function Atualizar() {
+        LimparTela();
+        Desenhar();
+    }
+
+    Iniciar();
+
+})();
